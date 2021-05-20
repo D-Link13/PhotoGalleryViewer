e2061d1fcd09e7edca32400b049f94c244d5eca8
Dima Tsurkan <d-link13@mail.ru>
2021-05-20 23:55:45 +0300

    added photo library accessing code and apply filters code

--- PhotoGalleryViewer/PhotoGalleryViewer/PhotoImageTableViewCell.swift
+++ PhotoGalleryViewer/PhotoGalleryViewer/PhotoImageTableViewCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class PhotoImageTableViewCell: UITableViewCell {
   @IBOutlet weak var photoImageView: UIImageView!
+  var representedAssetIdentifier: String!
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    photoImageView.image = nil
+  }
+  
 }
